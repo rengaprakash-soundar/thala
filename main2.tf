4cb76d37d9da0e9c34a8c08658e529edc2fa7a93
rengaprakash-soundar <59519532+rengaprakash-soundar@users.noreply.github.com>
2020-02-27 09:24:09 +0530

    ddfvvfvsfd

--- main2.tf
+++ main2.tf
@@ -5,14 +5,14 @@ provider "azurerm" {
   features {}
 }
 resource "azurerm_resource_group" "example" {
-  name     =  data.azurerm_resource_group.example.name
+  name     =  var.resource_name
   location =  var.location
 }
 
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "example" {
-  name                = data.azurerm_virtual_network.example.name
-  resource_group_name = data.azurerm_virtual_network.example.resource_group_name
+  name                = var.vnet_name
+  resource_group_name = var.resource_name
   location            = var.location
   address_space       =var.address_space
 }
