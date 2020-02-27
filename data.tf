8ab1f1449357e474086e12c5ccedba8cfb6e6a82
rengaprakash-soundar <59519532+rengaprakash-soundar@users.noreply.github.com>
2020-02-27 09:22:53 +0530

    dsss

--- data.tf
+++
@@ -1,7 +0,0 @@
-data "azurerm_resource_group" "example" {
-  name = "dsrg_test"
-}
-data "azurerm_virtual_network" "example" {
-  name                = "production"
-  resource_group_name = "dsrg_test"
-}
